a414be860292647d8f0e1d99de9c6265761e4299
Mayconn Cardoso Soares <mayconncardoso2015@gmail.com>
2024-09-12 21:50:58 -0300

    Atualização Busca de carros

--- BuscaCarros.php
+++ BuscaCarros.php
@@ -1,263 +1,111 @@
 <!DOCTYPE html>
-<html>
+<html lang="pt-BR">
+
 <head>
-  	<meta charset="utf-8">
-  	<meta name="viewport" content="initial-scale=1, width=device-width">
-  	
-  	<link rel="stylesheet"  href="./BuscaCarros.css" />
-  	<link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;700&display=swap" />
-  	
-  	
-  	
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
 </head>
+
 <body>
-  	
-  	<div class="desktop-10">
-    		<div class="fluent-mdl2contact">
-    		</div>
-    		<div class="desktop-10-child">
-    		</div>
-    		<div class="home">Home</div>
-    		<div class="nossos-veculos">Nossos veículos</div>
-    		<div class="sobre-ns">Sobre nós</div>
-    		<div class="desktop-10-item">
-    		</div>
-    		<div class="logo">Logo</div>
-    		<div class="desktop-10-inner">
-    		</div>
-    		<div class="rectangle-div">
-    		</div>
-    		<div class="desktop-10-child1">
-    		</div>
-    		<b class="escolha-o-grupo">Escolha o grupo de carros que melhor te atende </b>
-    		<div class="desktop-10-child2">
-    		</div>
-    		<img class="weuiarrow-filled-icon" alt="" src="images/icons/arrow-icon.svg">
-    		
-    		<div class="filtros-de-busca">Filtros de busca </div>
-    		<div class="tipo-de-veiculo">Tipo de Veiculo:</div>
-    		<div class="sedan">Sedan</div>
-    		<div class="hatch">Hatch</div>
-    		<div class="desktop-10-child3">
-    		</div>
-    		<div class="desktop-10-child4">
-    		</div>
-    		<div class="picapes">Picapes</div>
-    		<div class="esportivo">Esportivo</div>
-    		<div class="desktop-10-child5">
-    		</div>
-    		<div class="desktop-10-child6">
-    		</div>
-    		<div class="minivan">Minivan</div>
-    		<div class="suv">SUV</div>
-    		<div class="desktop-10-child7">
-    		</div>
-    	
-    		<div class="bagageiro">Bagageiro:</div>
-    		<div class="nmero-de-malas">Número de malas</div>
-    		<div class="desktop-10-child8">
-    		</div>
-    		<div class="filtrar">FILTRAR</div>
-    		<div class="desktop-10-child9">
-    		</div>
-    		<div class="desktop-10-child10">
-    		</div>
-    		<div class="alugar-agora">Alugar agora</div>
-    		<div class="r-110dia">
-      			<span>R$ 110</span>
-      			<span class="dia">/dia</span>
-    		</div>
-    		<img class="tablermanual-gearbox-filled-icon" alt="" src="images/icons/gearbox-icon.svg">
-    	
-			<img class="material-symbols-lighttravel-icon" alt="" src="images/icons/travel-icon.svg">
-    		
-    		<div class="div">5</div>
-    		<div class="m">M</div>
-    		<div class="div1">4</div>
-    		<div class="s">S</div>
-    		<img class="material-symbols-lightperson-icon" alt="" src="images/icons/person-icon.svg">
-    		
-    		<img class="unsplashr-i6y3efrxe-icon" alt="" src="images/pictures/car1.png">
-    		
-    		<b class="resultados">6 resultados</b>
-    		<img class="rectangle-icon" alt="" src="Rectangle 38.svg">
-    		
-    		<b class="mitsubishi-pajero">Mitsubishi Pajero</b>
-    		<img class="hugeiconssnow" alt="" src="images/icons/snow-icon.svg">
-    		
-    		<div class="desktop-10-child11">
-    		</div>
-    		<div class="desktop-10-child12">
-    		</div>
-    		<div class="alugar-agora1">Alugar agora</div>
-    		<div class="r-60dia">
-      			<span>R$ 60</span>
-      			<span class="dia">/dia</span>
-    		</div>
-    		<img class="tablermanual-gearbox-filled-icon1" alt="" src="images/icons/gearbox-icon.svg">
-    		
-    		<img class="material-symbols-lighttravel-icon1" alt="" src="images/icons/travel-icon.svg">
-    		
-    		<div class="div2">5</div>
-    		<div class="m1">M</div>
-    		<div class="div3">2</div>
-    		<div class="s1">S</div>
-    		<img class="material-symbols-lightperson-icon1" alt="" src="images/icons/person-icon.svg">
-    		
-    		<img class="desktop-10-child13" alt="" src="Rectangle 42.svg">
-    		
-    		<b class="volkswagen-gol">Volkswagen Gol</b>
-    		<img class="hugeiconssnow1" alt="" src="images/icons/snow-icon.svg">
-    		
-    		<div class="desktop-10-child14">
-    		</div>
-    		<div class="desktop-10-child15">
-    		</div>
-    		<div class="alugar-agora2">Alugar agora</div>
-    		<div class="r-115dia">
-      			<span>R$ 115</span>
-      			<span class="dia">/dia</span>
-    		</div>
-    		<img class="tablermanual-gearbox-filled-icon2" alt="" src="images/icons/gearbox-icon.svg">
-    		
-    		<img class="material-symbols-lighttravel-icon2" alt="" src="images/icons/travel-icon.svg">
-    		
-    		<div class="div4">5</div>
-    		<div class="a">A</div>
-    		<div class="div5">3</div>
-    		<div class="s2">S</div>
-    		<img class="material-symbols-lightperson-icon2" alt="" src="images/icons/person-icon.svg">
-    		
-    		<b class="toyota-corolla">Toyota Corolla</b>
-    		<img class="hugeiconssnow2" alt="" src="images/icons/snow-icon.svg">
-    		
-    		<div class="desktop-10-child16">
-    		</div>
-    		<div class="desktop-10-child17">
-    		</div>
-    		<div class="alugar-agora3">Alugar agora</div>
-    		<div class="r-220dia">
-      			<span>R$ 220</span>
-      			<span class="dia">/dia</span>
-    		</div>
-    		<img class="tablermanual-gearbox-filled-icon3" alt="" src="images/icons/gearbox-icon.svg">
-    		
-    		<img class="material-symbols-lighttravel-icon3" alt="" src="images/icons/travel-icon.svg">
-    		
-    		<div class="div6">5</div>
-    		<div class="a1">A</div>
-    		<div class="div7">2</div>
-    		<div class="s3">S</div>
-    		<img class="material-symbols-lightperson-icon3" alt="" src="images/icons/person-icon.svg">
-    		
-    		<b class="bmw-m3">BMW M3</b>
-    		<img class="hugeiconssnow3" alt="" src="images/icons/snow-icon.svg">
-    		
-    		<div class="desktop-10-child18">
-    		</div>
-    		<div class="desktop-10-child19">
-    		</div>
-    		<div class="alugar-agora4">Alugar agora</div>
-    		<div class="r-125dia">
-      			<span>R$ 125</span>
-      			<span class="dia">/dia</span>
-    		</div>
-    		<img class="tablermanual-gearbox-filled-icon4" alt="" src="images/icons/gearbox-icon.svg">
-    		
-    		<img class="material-symbols-lighttravel-icon4" alt="" src="images/icons/travel-icon.svg">
-    		
-    		<div class="div8">5</div>
-    		<div class="a2">A</div>
-    		<div class="div9">3</div>
-    		<div class="s4">S</div>
-    		<img class="material-symbols-lightperson-icon4" alt="" src="images/icons/person-icon.svg">
-    		
-    		<b class="honda-civic">Honda Civic</b>
-    		<img class="hugeiconssnow4" alt="" src="images/icons/snow-icon.svg">
-    		
-    		<div class="desktop-10-child20">
-    		</div>
-    		<div class="desktop-10-child21">
-    		</div>
-    		<div class="alugar-agora5">Alugar agora</div>
-    		<div class="r-140dia">
-      			<span>R$ 140</span>
-      			<span class="dia">/dia</span>
-    		</div>
-    		<img class="tablermanual-gearbox-filled-icon5" alt="" src="images/icons/gearbox-icon.svg">
-    		
-    		<img class="material-symbols-lighttravel-icon5" alt="" src="images/icons/travel-icon.svg">
-    		
-    		<div class="div10">5</div>
-    		<div class="a3">A</div>
-    		<div class="div11">3</div>
-    		<div class="s5">S</div>
-    		<img class="material-symbols-lightperson-icon5" alt="" src="images/icons/person-icon.svg">
-    		
-    		<b class="ford-fusion">Ford Fusion</b>
-    		<img class="hugeiconssnow5" alt="" src="images/icons/snow-icon.svg">
-    		
-    		<img class="unsplash8xyki0bqvgw-icon" alt="" src="images/pictures/car2.png">
-    		
-    		<img class="desktop-10-child22" alt="" src="Rectangle 64.svg">
-    		
-    		<img class="unsplashdwnmxurb1tk-icon" alt="" src="images/pictures/car3.png">
-    		
-    		<div class="desktop-10-child23">
-    		</div>
-    		<div class="desktop-10-child24">
-    		</div>
-    		<div class="local-de-retirada">Local de retirada</div>
-    		<img class="unsplashz6rvfpkqvqg-icon" alt="" src="images/pictures/car5.png">
-    		
-    		<div class="desktop-10-child25">
-    		</div>
-    		<div class="desktop-10-child26">
-    		</div>
-    		<div class="data-de-retirada">Data de retirada</div>
-    		<div class="hora-de-retirada">Hora de retirada</div>
-    		<img class="icon" alt="" src="images/icons/calendar-icon.svg">
-    		
-    		<img class="icon1" alt="" src="images/icons/time-icon.svg">
-    		
-    		<div class="desktop-10-child27">
-    		</div>
-    		<div class="desktop-10-child28">
-    		</div>
-    		<div class="data-de-devoluo">Data de devolução</div>
-    		<div class="hora-de-devoluo">Hora de devolução</div>
-    		<img class="icon2" alt="" src="images/icons/calendar-icon.svg">
-    		
-      		<img class="div12" alt="" src="images/icons/time-icon.svg">
-		
-    		<div class="desktop-10-child29">
-    		</div>
-    		<div class="continuar">Continuar</div>
-    		<img class="unsplash27cpg3pdoue-icon" alt="" src="images/pictures/car4.png">
-    		
-    		<img class="desktop-10-child30" alt="" src="Rectangle 67.svg">
-    		
-    		
-    		<img class="desktop-10-child32" alt="" src="Rectangle 77.svg">
-    		
-    		<img class="unsplashmf7olmgnzee-icon" alt="" src="images/pictures/car6.png">
-    		
-    		<img class="desktop-10-child33" alt="" src="Rectangle 78.svg">
-    		
-    		<img class="mdilocation-icon" alt="" src="images/icons/location-icon.svg">
-    		
-    		<div class="desktop-10-child34">
-    		</div>
-    		<div class="inscrever-se">Inscrever-se</div>
-    		<div class="desktop-10-child35">
-    		</div>
-    		<div class="entrar">Entrar</div>
-    		<img class="pepicons-popperson" alt="" src="images/icons/person-icon.svg">
-    		
-  	</div>
-  	
-  	
-  	
-  	
+    <div class="desktop">
+        <div class="header">
+            <div class="logo">Logo</div>
+            <div class="nav-buttons">
+                <a href="#home">Home</a>
+                <a href="#vehicles">Nossos veículos</a>
+                <a href="#about">Sobre nós</a>
+            </div>
+            <div class="auth-buttons">
+                <a href="#signup" class="button">Inscrever-se</a>
+                <div class="separator"></div>
+                <a href="#login" class="button">Entrar</a>
+            </div>
+        </div>
+        <div class="overlap-wrapper">
+            <div class="overlap">
+                <div class="overlap-group">
+                    <input type="text" class="input-field-local" placeholder="Local de retirada">
+                </div>
+
+                <!-- Campos de Data e Hora de Retirada -->
+                <div class="div">
+                    <div class="input-group">
+                        <input type="text" id="date-picker" class="input-field left" placeholder="Data de retirada">
+                        <input type="text" id="time-picker" class="input-field right" placeholder="Hora de retirada">
+                    </div>
+                </div>
+
+                <!-- Campos de Data e Hora de Devolução -->
+                <div class="overlap-2">
+                    <div class="input-group">
+                        <input type="text" id="date-return" class="input-field left" placeholder="Data de devolução">
+                        <input type="text" id="time-return" class="input-field right" placeholder="Hora de devolução">
+                    </div>
+                </div>
+
+                <!-- Botão Continuar -->
+                <div class="div-wrapper">
+                    <button class="continue-button">Continuar</button>
+                </div>
+            </div>
+        </div>
+        <div class="filter-container">
+            <!-- Novo texto adicionado -->
+            <div class="group-selection">Escolha o grupo de carros que melhor te atende</div>
+            <div class="filter-header" id="filter-header">
+                <h2>Filtros de busca</h2>
+                <div class="toggle-icon" id="toggle-icon">
+                    <img src="images/icons/arrow-icon.svg" alt="Toggle Icon">
+                </div>
+            </div>
+            <div class="filter-panel" id="filter-panel">
+            <div class="filter-group-container">
+                <div class="filter-group">
+                    <div class="filter-label">Tipo de Veículo:</div>
+                    <div class="filter-options">
+                        <label>
+                            <input type="checkbox" class="filter-checkbox">
+                            Sedan
+                        </label>
+                        <label>
+                            <input type="checkbox" class="filter-checkbox">
+                            Hatch
+                        </label>
+                        <label>
+                            <input type="checkbox" class="filter-checkbox">
+                            Minivan
+                        </label>
+                        <label>
+                            <input type="checkbox" class="filter-checkbox">
+                            SUV
+                        </label>
+                        <label>
+                            <input type="checkbox" class="filter-checkbox">
+                            Picapes
+                        </label>
+                        <label>
+                            <input type="checkbox" class="filter-checkbox">
+                            Esportivo
+                        </label>
+                    </div>
+                </div>
+                <div class="filter-group">
+                    <div class="filter-label">Bagageiro:</div>
+                    <select>
+                        <option value="">Número de malas</option>
+                        <option value="1">1 mala</option>
+                        <option value="2">2 malas</option>
+                        <option value="3">3 malas</option>
+                        <option value="4">4 malas</option>
+                        <option value="5">5 malas</option>
+                    </select>
+                    <button class="filter-button">FILTRAR</button>
+                </div>
+            </div>
+        </div>   
+    </div>
+    <script src="script.js"></script>
 </body>
 </html>
