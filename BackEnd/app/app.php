c00a775730a505d05908b1afe531b5d550a12c25
joaovitro99 <85196542+joaovitro99@users.noreply.github.com>
2024-09-23 19:30:57 -0300

    Create app.php

---
+++ BackEnd/app/app.php
@@ -0,0 +1,12 @@
+<?php
+
+
+include("mySqlDataProvider.php");
+include("UserRepository.php");
+include("CarRepository.php");
+include("config.php");
+
+$data_provider= new MySqlDataProvider($config);
+$user_repository= new UserRepository($data_provider);
+$car_repository= new CarRepository($data_provider);
+// .....
