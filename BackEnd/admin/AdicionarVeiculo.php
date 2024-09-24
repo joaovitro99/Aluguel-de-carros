67b1bf93fdbcf708b6e77f922e69bd388a60ca70
joaovitro99 <joaoreisreis99@gmail.com>
2024-09-24 10:58:56 -0300

    css deu bom

--- BackEnd/admin/AdicionarVeiculo.php
+++ BackEnd/admin/AdicionarVeiculo.php
@@ -46,14 +46,14 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     if (!empty($_POST['placa'])) {
         $placa = strtoupper(trim($_POST['placa']));
       
-        if (!preg_match('/^[A-Z]{3}-?[0-9]{4}$/', $placa) && !preg_match('/^[A-Z]{3}[0-9][A-Z][0-9]{2}$/', $placa)) {
+        if (!preg_match('/^[A-Z]{3}[0-9]{4}$/', $placa) && !preg_match('/^[A-Z]{3}[0-9][A-Z][0-9]{2}$/', $placa)) {
             $errors[] = "Placa inválida.";
         }
     } else {
         $errors[] = "A placa é obrigatória.";
     }
 
-  
+    
     if (!empty($_POST['valorDiaria'])) {
         $valor_diaria = filter_var($_POST['valorDiaria'], FILTER_VALIDATE_FLOAT);
         if (!$valor_diaria || $valor_diaria <= 0) {
