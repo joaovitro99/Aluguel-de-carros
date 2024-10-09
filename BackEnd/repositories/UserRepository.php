5c10b60d9c4eb2b301c2f2390d87467ce9811950
joaovitro99 <joaoreisreis99@gmail.com>
2024-10-09 12:41:10 -0300

    hash

--- BackEnd/repositories/UserRepository.php
+++ BackEnd/repositories/UserRepository.php
@@ -29,37 +29,38 @@ class UserRepository{
     }
     public function getUserLogin($nome_usuario, $senha){
         $sql_login = "SELECT senha FROM usuarios WHERE nome_usuario = ?";
-$stmt_login = $this->data_provider->prepare($sql_login);
-$stmt_login->bind_param("s", $nome_usuario);
-$stmt_login->execute();
-$result_login = $stmt_login->get_result();
-$row = '';
-
-if ($result_login->num_rows > 0) {
-    $row = $result_login->fetch_assoc();
-    $hashed_senha = $row['senha'];
-
-    // Verifica a senha
-    if (password_verify($senha, $hashed_senha)) {
-        // Senha correta
-        return $row;
+        $stmt_login = $this->data_provider->prepare($sql_login);
+        $stmt_login->bind_param("s", $nome_usuario);
+        $stmt_login->execute();
+        $result_login = $stmt_login->get_result();
+        $row = '';
         
-    } else {
-        // Senha incorreta
-        echo "<script>
-            alert('senha incorreta')
-            window.location.href = '../../FrontEnd/Login.php';
-          </script>";
-          exit();
-    }
-} else {
-    // Usuário não encontrado
-    echo "<script>
-            alert('Usuario incorreto')
-            window.location.href = '../../FrontEnd/Login.php';
-          </script>";
-          exit();
-}
+        if ($result_login->num_rows > 0) {
+            $row = $result_login->fetch_assoc();
+            $hashed_senha = $row['senha'];
+        
+            // Verifica a senha
+            if (password_verify($senha, $hashed_senha)) {
+                // Senha correta
+                return $row;
+                
+            } else {
+                // Senha incorreta
+                echo "<script>
+                    alert('senha incorreta')
+                    window.location.href = '../../FrontEnd/Login.php';
+                  </script>";
+                  exit();
+            }
+        } else {
+            // Usuário não encontrado
+            echo "<script>
+                    alert('Usuario incorreto')
+                    window.location.href = '../../FrontEnd/Login.php';
+                  </script>";
+                  exit();
+        }
+        
         
 
         
