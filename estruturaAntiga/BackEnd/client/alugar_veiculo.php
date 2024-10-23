0d68029367c7f1e136b1f038a1993f968ebfe93d
VicthorCM <126515645+VicthorCM@users.noreply.github.com>
2024-10-23 08:15:25 -0300

    apagando estrutura antiga

--- estruturaAntiga/BackEnd/client/alugar_veiculo.php
+++
@@ -1,8 +0,0 @@
-<?php
-include("mySqlDataProvider.php");
-include("UserRepository.php");
-include("CarRepository.php");
-include("config.php");
-
-$sql = "SELECT marca, modelo, ano,valor_diaria FROM veiculos WHERE status = 'disponível' ";
-$result = $conexao->query($sql);
